d6050e03da8ce64132fbea1850ecfc039505f45a
Cinthia Ramos <cinthia24027@gmail.com>
2022-04-21 07:39:59 -0500

    creando 3 promesas, y ejecutándolas una por una

--- Learning promise.js
+++ Learning promise.js
@@ -5,5 +5,25 @@ const p1 = new Promise(function(resolve, reject) {
    }, 1000);
 });
 
-p1.then(() => console.log(p1));
+// p1.then(() => console.log(p1));
+
+const p2 = new Promise(function(resolve, reject) {
+   setTimeout(function() {
+       console.log('p2 ejecutado');
+       resolve({num: 2});
+   }, 2000);
+   });
+   
+   
+   const p3 = new Promise(function(resolve, reject) {
+       setTimeout(function() {
+           console.log('p3 ejecutado');
+           resolve({num: 3});
+       }, 3000);
+       })
+   
+   // Solo cuando se resuelve la promesa 3 podemos obtener el resultado de las 3 promesas juntas.
+   p1.then(() => console.log(p1, p2, p3));
+   p2.then(() => console.log(p1, p2, p3));
+   p3.then(() => console.log(p1, p2, p3));
  
